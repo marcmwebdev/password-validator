c7aeb5c2082764b8bf84386b3edc06e2e1724f20
Marc M <marcmwebdev@gmail.com>
2021-12-13 04:28:31 -0500

    v2 changes made

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
-const validatePassword = (somePassword) => {
+// const validatePassword = (somePassword) => {
   
-  let hasLowerCase = false;
-  let hasUpperCase = false;
-  let hasNumeric = false;
-  let hasSpecial = false;
+//   let hasLowerCase = false;
+//   let hasUpperCase = false;
+//   let hasNumeric = false;
+//   let hasSpecial = false;
   
-  if (somePassword.length < 8) {
-    return false;
-  }  
+//   if (somePassword.length < 8) {
+//     return false;
+//   }  
 
-  for (let i = 0; i < somePassword.length; i++) {
-    const isLower = /^[a-z]*$/.test(somePassword[i]);
-    const isUpper = /^[A-Z]*$/.test(somePassword[i]);
-    const isNumeric = /\d/.test(somePassword[i]);
-    const isSpecial = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(somePassword[i]);
+//   for (let i = 0; i < somePassword.length; i++) {
+//     const isLower = /^[a-z]*$/.test(somePassword[i]);
+//     const isUpper = /^[A-Z]*$/.test(somePassword[i]);
+//     const isNumeric = /\d/.test(somePassword[i]);
+//     const isSpecial = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(somePassword[i]);
       
-    if (isSpecial) hasSpecial = true;
-    if (isLower) hasLowerCase = true;
-    if (isUpper) hasUpperCase = true;
-    if (isNumeric) hasNumeric = true;
-  }
+//     if (isSpecial) hasSpecial = true;
+//     if (isLower) hasLowerCase = true;
+//     if (isUpper) hasUpperCase = true;
+//     if (isNumeric) hasNumeric = true;
+//   }
   
-  if (hasLowerCase && hasUpperCase && hasNumeric && hasSpecial) {
-    return true;
-  }
+//   if (hasLowerCase && hasUpperCase && hasNumeric && hasSpecial) {
+//     return true;
+//   }
   
-  return false;
+//   return false;
 
+// }
+
+
+
+function isLowerCase(pwd, index) {
+  return pwd.charCodeAt(index) >= 97 && pwd.charCodeAt(index) <= 122
+}
+console.log(isLowerCase('hELLO', 0)) // should return true
+console.log(isLowerCase('hEllo', 1)) // should return false
+
+
+
+function isUpperCase(pwd, index) {
+  return pwd.charCodeAt(index) >= 65 && pwd.charCodeAt(index) <= 90
+}
+console.log(isUpperCase('WElcome', 0)) // should return true
+console.log(isUpperCase('wElcome', 4)) // should return false
+
+
+
+function isNumeric(pwd, index) {
+  return !isNaN(pwd[index])
+}
+console.log(isNumeric('ppp4', 3)) // should return true
+console.log(isNumeric('pppp%', 0)) // should return false
+
+
+
+function validatePassword(pwd) {
+  
+  if (pwd.length < 8) return false
+  
+  let upper = 0
+  let lower = 0
+  let numeric = 0
+  let special = 0
+
+ 
+  for (let i = 0; i < pwd.length; i++) {
+    if (isLowerCase(pwd, i)) {
+      lower++
+    } else if (isUpperCase(pwd, i)) {
+      upper++
+    } else if (isNumeric(pwd, i)) {
+      numeric++
+    } else {
+      special++
+    }
+  }
+
+  
+  return lower > 0 && upper > 0 && numeric > 0 && special > 0
 }
 
-
-module.exports = validatePassword
+ module.exports = validatePassword
 
